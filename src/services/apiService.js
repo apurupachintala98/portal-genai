7e8ff7f4534989e78748a99e1eb04384acc32ff0
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-01-08 18:57:06 +0530

    push

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -61,7 +61,7 @@ export const updateProjectDetails = async (sl_no, updatedProject) => {
 // Delete project details
 export const deleteProjectDetails = async (sl_no) => {
   try {
-    const response = await axios.post(`${Dashboard_BASE_URL}/delete_project_details/`, sl_no);
+    const response = await axios.post(`${Dashboard_BASE_URL}/delete_project_details/?sl_no=${sl_no}`);
     return response.data;
   } catch (error) {
     console.error("Error deleting project details:", error);
