379974cdecdfeec5936da1f220cee19d077ee860
apurupachintala95 <89081384+apurupachintala95@users.noreply.github.com>
2025-02-07 14:47:31 +0530

    push

--- src/components/RedirectComponent.jsx
+++ src/components/RedirectComponent.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 const RedirectComponent = ({ src }) => {
-  return (
-    <div style={{ width: '100%', height: '100vh' }}>
-      <iframe src={src} title="External Content" style={{ width: '100%', height: '100%', border: 'none' }} />
-    </div>
-  );
-};
-
-export default RedirectComponent;
+    return (
+      <div style={{ width: '100%', height: '100vh' }}>
+        <iframe src={src} title="External Content" style={{ width: '100%', height: '100%', border: 'none' }} />
+      </div>
+    );
+  };
+  
+  export default RedirectComponent;
+  
